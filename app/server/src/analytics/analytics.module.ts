39b75829bf3c0b9d8a9d37a12497cc41d450a016
Bohdan <bogdankruk29@gmail.com>
2025-08-13 16:36:26 +0300

    Fix module server architechture

--- app/server/src/analytics/analytics.module.ts
+++ app/server/src/analytics/analytics.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AnalyticsService } from './services/analytics/analytics.service';
-import { AnalyticsGateway } from './gateways/analytics/analytics.gateway';
-import { AnalyticsController } from './controllers/analytics/analytics.controller';
+import { AnalyticsService } from './services/analytics.service';
+import { AnalyticsGateway } from './gateways/analytics.gateway';
+import { AnalyticsController } from './controllers/analytics.controller';
 
 @Module({
   providers: [AnalyticsService, AnalyticsGateway],
